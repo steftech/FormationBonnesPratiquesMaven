8e4830fce974311156099e49db2e3a7192ef2b78
Stéphane LARSON <stephane.larson@gmail.com>
2026-01-27 15:32:38 +0100

    Correction

--- src/main/java/chap2/exercice_refactoring_lambda/Main.java
+++ src/main/java/chap2/exercice_refactoring_lambda/Main.java
@@ -5,34 +5,37 @@ import java.util.List;
 
 public class Main {
     static void main() {
-        List<PointColore> listePoints = new ArrayList<>( List.of(
-                new PointColore(0,5,Couleur.Blanc)
-                ,new PointColore(-5,0,Couleur.Jaune)
-                ,new PointColore(5,7,Couleur.Vert)
-                ,new PointColore(10,25,Couleur.Noir)
-                ,new PointColore(7,5,Couleur.Jaune)
-                ,new PointColore(107,5,Couleur.Jaune)
-                ,new PointColore(7,505,Couleur.Jaune)
-                ,new PointColore(17,5,Couleur.Jaune)
-                ,new PointColore(7,55,Couleur.Jaune)
-                ,new PointColore(10,5,Couleur.Jaune)
-                ,new PointColore(7,5,Couleur.Jaune)
+        List<Point> listePoints = new ArrayList<>(List.of(
+                new PointColore(0, 5, Couleur.Blanc)
+                , new PointColore(-5, 0, Couleur.Jaune)
+                , new Point(-25, 0)
+                , new PointColore(5, 7, Couleur.Vert)
+                , new PointColore(10, 25, Couleur.Noir)
+                , new PointColore(7, 5, Couleur.Jaune)
+                , new PointColore(107, 5, Couleur.Jaune)
+                , new Point(-15, 17)
+                , new PointColore(7, 505, Couleur.Jaune)
+                , new PointColore(17, 5, Couleur.Jaune)
+                , new PointColore(7, 55, Couleur.Jaune)
+                , new PointColore(10, 5, Couleur.Jaune)
+                , new PointColore(7, 5, Couleur.Jaune)
         ));
 
-        List<PointColore> listePointsColore = getTop5PointsJaunesProcheOrigine(listePoints);
-        afficheListe(listePointsColore);
+        List<Double> listePointsFiltre = getTop5PointsJaunesProcheOrigine(listePoints);
+        afficheListe(listePointsFiltre);
 
     }
 
-    private static List<PointColore> getTop5PointsJaunesProcheOrigine(List<PointColore> listePoints) {
+    private static List<Double> getTop5PointsJaunesProcheOrigine(List<Point> listePoints) {
         return listePoints.stream()
-                .filter(p-> p.getCouleur() == Couleur.Jaune)
+                .filter(p -> p instanceof PointColore pc && pc.getCouleur() == Couleur.Jaune)
                 .sorted((p1, p2) -> Double.compare(p1.getDistance(), p2.getDistance()))
+                .map(p -> p.getDistance())
                 .limit(5)
                 .toList();
     }
 
-    private static<T> void afficheListe(List<T> liste) {
+    private static <T> void afficheListe(List<T> liste) {
         String sep = "";
         for (T p : liste) {
             System.out.print(sep + p + ",");
